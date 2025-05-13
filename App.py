5ef44a8e29c6569eddc38583a99964bb55bcd632
SERGIO PAIVA DE CAMPOS <32659392+sergiokmpos@users.noreply.github.com>
2025-05-13 22:56:05 +0000

    Build 0.1

--- App.py
+++ App.py
@@ -1,4 +1,23 @@
 import streamlit as st
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain. chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+from longchain_community.document_loaders import CSVLoader
 
+load_dotenv()
+
+loader = CSVLoader(file_path="Content.csv")
+documents = loader.load()
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
+
+
+def retrieve(query):
+    similar_response = db.similarity_search(query, k=3)
+    return [doc.page_content for doc in similar_response]
+
+                   
